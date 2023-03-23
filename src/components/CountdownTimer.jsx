8e2384e2cc8bb4eb167ceaf4de5e08bb6f5aeceb
corjon <cordesjonathan@gmail.com>
2023-03-23 16:18:44 -0700

    Add countdown timer

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -1,23 +1,50 @@
+import { useState, useEffect } from 'react';
 import styles from './CountdownTimer.module.css';
 
 export default function CountdownTimer() {
+  const [countdown, setCountdown] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + 30);
+
+  const targetDateString = targetDate.toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date().getTime();
+      const timeRemaining = targetDate.getTime() - now;
+
+      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+      
+      setCountdown({ days, hours, minutes, seconds });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className={styles.timer}>
-      <div>
-        <p>47</p> 
-        <p>Days</p>
-      </div>
-      <div>
-        <p>07</p>
-        <p>Hours</p>
-      </div>
-      <div>
-        <p>56</p> 
-        <p>Min</p>
-      </div>
-      <div>
-        <p>14</p>
-        <p>Sec</p>
+    <div>
+      <p className={styles.comingSoon}>Coming <span>{targetDateString}</span></p>
+      <div className={styles.timer}>
+        <div>
+          <p>{countdown.days}</p>
+          <p>Days</p>
+        </div>
+        <div>
+          <p>{countdown.hours}</p>
+          <p>Hours</p>
+        </div>
+        <div>
+          <p>{countdown.minutes}</p>
+          <p>Min</p>
+        </div>
+        <div>
+          <p>{countdown.seconds}</p>
+          <p>Sec</p>
+        </div>
       </div>
     </div>
   );
