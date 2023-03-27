26eb51d60207a9ca094862ddfc60bbbc13043671
corjon <cordesjonathan@gmail.com>
2023-03-27 16:32:04 -0700

    Add styling for SignUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,13 @@
 import logo from '../assets/shared/logo.svg';
 import CountdownTimer from '../components/CountdownTimer.jsx';
 import SignUpForm from '../components/SignUpForm.jsx';
+import styles from './SignUp.module.css';
 
 export default function SignUp() {
   return (
-    <main>
+    <main className={styles.signUpContent}>
       <img src={logo} alt="officelite logo" />
-      <div>
+      <div className={styles.signUpBanner}>
         <h1>Work smarter. Save time.</h1>
 
         <p>Easily manage your projects. Get on the list and receive in-app perks available only to early subscribers. We are moving into final development and getting ready for official launch soon.</p>
@@ -14,7 +15,7 @@ export default function SignUp() {
         <CountdownTimer />
       </div>
 
-      <div>
+      <div className={styles.signUpSection}>
         <SignUpForm />
       </div>
   </main>
