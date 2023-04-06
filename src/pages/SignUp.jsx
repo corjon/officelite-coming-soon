3ef3e84c93b6059dab19b5265517b20f63c5f5a8
corjon <cordesjonathan@gmail.com>
2023-04-06 13:22:59 -0700

    Move logo img into div

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,8 +6,8 @@ import styles from './SignUp.module.css';
 export default function SignUp() {
   return (
     <main className={styles.signUpContent}>
-      <img src={logo} alt="officelite logo" />
       <div className={styles.signUpBanner}>
+        <img src={logo} alt="officelite logo" />
         <h1>Work smarter. Save time.</h1>
 
         <p>Easily manage your projects. Get on the list and receive in-app perks available only to early subscribers. We are moving into final development and getting ready for official launch soon.</p>
