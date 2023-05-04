cbe2503c356093093ece9ed0f07be7d0c1129499
corjon <cordesjonathan@gmail.com>
2023-05-04 16:05:20 -0700

    Add basic form validation

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,17 +1,28 @@
 import styles from './SignUpForm.module.css';
+import { useForm } from 'react-hook-form';
 
 export default function SignUpForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
   return (
-    <form className={styles.form}>
-      <input type="text" name="name" aria-label="Name" placeholder="Name"></input>
-      <input type="text" name="email" aria-label="Email Address" placeholder="Email Address"></input>
-      <select name="plan" aria-label="Select a plan">
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" name="name" aria-label="Name" placeholder="Name" {...register("name", { required: true })}></input>
+      <input type="text" name="email" aria-label="Email Address" placeholder="Email Address" 
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}>
+      </input>
+      <select name="plan" aria-label="Select a plan" {...register("plan", { required: true })}>
         <option value="basic">Basic Pack Free</option>
         <option value="pro">Pro Pack $9.99</option>
         <option value="ultimate">Ultimate Pack $19.99</option>
       </select>
-      <input type="text" name="phone" aria-label="Phone Number" placeholder="Phone Number"></input>
-      <input type="text" name="company" aria-label="Company" placeholder="Company"></input>
+      <input type="text" name="phone" aria-label="Phone Number" placeholder="Phone Number" 
+        {...register("phone", { required: true, pattern: /^[0-9]+$/i })}>
+      </input>
+      <input type="text" name="company" aria-label="Company" placeholder="Company" {...register("company", { required: true })}></input>
       <button type="submit">Get on the list</button>
     </form>
   );
